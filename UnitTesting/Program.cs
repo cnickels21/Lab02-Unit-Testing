e4b84f9bc9d13a28b54d05b61ff013c8f252e88e
cnickels21 <cnickels_21@yahoo.com>
2020-05-05 21:40:22 -0500

    try catch added in atm handler

--- UnitTesting/Program.cs
+++ UnitTesting/Program.cs
@@ -9,6 +9,7 @@ namespace UnitTesting
             Console.WriteLine("Welcome to my ATM simulation!");
 
             AtmTransactionHandler(500);
+            
         }
 
         public static int TestingTheTest(int num)
@@ -23,37 +24,44 @@ namespace UnitTesting
             // Initiate user balance as 0
             decimal userBalance = currentBalance;
 
-            Console.WriteLine("To check your balance enter 1.\nTo withdraw money enter 2.\nTo deposit money enter 3.\nTo exit enter \"4\".");
+            try
+            {
+                Console.WriteLine("To check your balance enter 1.\nTo withdraw money enter 2.\nTo deposit money enter 3.\nTo exit enter \"4\".");
             int selectedTransaction = int.Parse(Console.ReadLine());
 
-            if (selectedTransaction == 1)
+            
+                if (selectedTransaction == 1)
+                {
+                    Console.WriteLine($"\nYour balance is: {userBalance}\n");
+                    AtmTransactionHandler(userBalance);
+                }
+                if (selectedTransaction == 2)
+                {
+                    Console.WriteLine("\nHow much would you like to withdraw: \n");
+                    decimal withdrawalAmount = decimal.Parse(Console.ReadLine());
+                    userBalance = WithdrawalHandler(userBalance, withdrawalAmount);
+                    Console.WriteLine($"\nYour balance is now: {userBalance}\n");
+                    AtmTransactionHandler(userBalance);
+                }
+                if (selectedTransaction == 3)
+                {
+                    Console.WriteLine("\nHow much would you like to deposit: \n");
+                    decimal depositAmount = decimal.Parse(Console.ReadLine());
+                    userBalance = DepositHandler(userBalance, depositAmount);
+                    Console.WriteLine($"\nYour balance is now: {userBalance}\n");
+                    AtmTransactionHandler(userBalance);
+                }
+                if (selectedTransaction == 4)
+                {
+                    return;
+                }
+            }
+            catch (FormatException fex)
             {
-                Console.WriteLine($"\nYour balance is: {userBalance}\n");
+                Console.WriteLine("\nPlease enter a number!\n");
                 AtmTransactionHandler(userBalance);
             }
-            if (selectedTransaction == 2)
-            {
-                Console.WriteLine("\nHow much would you like to withdraw: \n");
-                decimal withdrawalAmount = decimal.Parse(Console.ReadLine());
-                userBalance = WithdrawalHandler(userBalance, withdrawalAmount);
-                Console.WriteLine($"\nYour balance is now: {userBalance}\n");
-                AtmTransactionHandler(userBalance);
-            }
-            if (selectedTransaction == 3)
-            {
-                Console.WriteLine("\nHow much would you like to deposit: \n");
-                decimal depositAmount = decimal.Parse(Console.ReadLine());
-                userBalance = DepositHandler(userBalance, depositAmount);
-                Console.WriteLine($"\nYour balance is now: {userBalance}\n");
-                AtmTransactionHandler(userBalance);
-            }
-            if (selectedTransaction == 4)
-            {
-                return;
-            }
-
-
-
+            
 
         }
 
