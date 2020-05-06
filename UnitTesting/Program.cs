2f6f56005decb8a5bb1f1a653ea83d90dc1f20c0
cnickels21 <cnickels_21@yahoo.com>
2020-05-05 19:34:49 -0500

    atm handler set up to exit on specific user input

--- UnitTesting/Program.cs
+++ UnitTesting/Program.cs
@@ -8,11 +8,40 @@ namespace UnitTesting
         {
             Console.WriteLine("Welcome to my ATM simulation!");
 
+            AtmTransactionHandler();
         }
 
         public static int TestingTheTest(int num)
         {
             return num + 1;
         }
+
+        public static void AtmTransactionHandler()
+        {
+            // Continue to ask for transaction type until exit is chosen
+            while (true)
+            {
+                try
+                {
+                    // Initiate user balance as 0
+                    int userBalance = 0;
+
+                    Console.WriteLine("To check your balance enter 1.\nTo withdraw money enter 2.\nTo deposit money enter 3.\nTo exit enter \"x\".");
+                    int selectedTransaction = int.Parse(Console.ReadLine());
+
+                }
+                catch (FormatException fex) // Throw back to main when user chooses exit
+                {
+                    throw fex;
+
+                }
+            }
+            
+        }
+
+
+
+
+
     }
 }
