88314e6bd1a28feaad8b23b1f854c157c1251430
cnickels21 <cnickels_21@yahoo.com>
2020-05-05 22:03:15 -0500

    git ignore\attribute

--- UnitTesting/Program.cs
+++ UnitTesting/Program.cs
@@ -19,60 +19,75 @@ namespace UnitTesting
 
         public static void AtmTransactionHandler(decimal currentBalance)
         {
-            // Continue to ask for transaction type until exit is chosen
-
-            // Initiate user balance as 0
+            // Initiate user balance
             decimal userBalance = currentBalance;
 
             try
             {
+                // Initial prompt for user transaction
                 Console.WriteLine("To check your balance enter 1.\nTo withdraw money enter 2.\nTo deposit money enter 3.\nTo exit enter \"4\".");
-            int selectedTransaction = int.Parse(Console.ReadLine());
+                int selectedTransaction = int.Parse(Console.ReadLine());
 
             
                 if (selectedTransaction == 1)
                 {
                     Console.WriteLine($"\nYour balance is: {userBalance}\n");
-                    AtmTransactionHandler(userBalance);
+                    AtmTransactionHandler(userBalance); // Keep atm open
                 }
-                if (selectedTransaction == 2)
+                if (selectedTransaction == 2) // Handler for withdrawals
                 {
-                    Console.WriteLine("\nHow much would you like to withdraw: \n");
-                    decimal withdrawalAmount = decimal.Parse(Console.ReadLine());
-                    userBalance = WithdrawalHandler(userBalance, withdrawalAmount);
-                    Console.WriteLine($"\nYour balance is now: {userBalance}\n");
-                    AtmTransactionHandler(userBalance);
+                    userBalance = WithdrawalHandler(userBalance);
+                    AtmTransactionHandler(userBalance); // Keep atm open with updated balance
                 }
-                if (selectedTransaction == 3)
+                if (selectedTransaction == 3)  // Handler for deposits
                 {
-                    Console.WriteLine("\nHow much would you like to deposit: \n");
-                    decimal depositAmount = decimal.Parse(Console.ReadLine());
-                    userBalance = DepositHandler(userBalance, depositAmount);
-                    Console.WriteLine($"\nYour balance is now: {userBalance}\n");
-                    AtmTransactionHandler(userBalance);
+                    userBalance = DepositHandler(userBalance);
+                    AtmTransactionHandler(userBalance); // Keep atm open with updated balance
                 }
-                if (selectedTransaction == 4)
+                if (selectedTransaction == 4) // Close when prompted
                 {
                     return;
                 }
             }
-            catch (FormatException fex)
+            catch (FormatException) // Asks the user for a valid input
             {
-                Console.WriteLine("\nPlease enter a number!\n");
+                Console.WriteLine("\nInvalid response. Please enter a number!\n");
                 AtmTransactionHandler(userBalance);
             }
             
 
         }
 
-        public static decimal DepositHandler(decimal currentBalance, decimal depositAmount)
+        public static decimal DepositHandler(decimal currentBalance)
         {
-            return currentBalance + depositAmount;
+            // Get deposit amount
+            Console.WriteLine("\nHow much would you like to deposit: \n");
+            decimal depositAmount = decimal.Parse(Console.ReadLine());
+
+            // Set new balance
+            decimal newBalance = currentBalance + depositAmount;
+
+            // Display new balance
+            Console.WriteLine($"\nYour balance is now: {newBalance}\n");
+
+            // Return to atm hanlder to update balance
+            return newBalance;
         }
 
-        public static decimal WithdrawalHandler(decimal currentBalance, decimal withdrawalAmount)
+        public static decimal WithdrawalHandler(decimal currentBalance)
         {
-            return currentBalance - withdrawalAmount;
+            // Get withdrawal amount
+            Console.WriteLine("\nHow much would you like to withdraw: \n");
+            decimal withdrawalAmount = decimal.Parse(Console.ReadLine());
+
+            // Set new balance
+            decimal newBalance = currentBalance - withdrawalAmount;
+
+            // Display new balance
+            Console.WriteLine($"\nYour balance is now: {newBalance}\n");
+
+            // Return to atm handler to update balance
+            return newBalance;
         }
 
     }
